bc757e436c66a28413557e764afce965ce45e095
Knifer <yeded73422@invodua.com>
2023-11-09 13:37:15 +0300

    slightly corrected the structure

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,23 +1,18 @@
 import "./Root.scss";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./MainPage/MainPage";
-import { QueryClient, QueryClientProvider } from "react-query";
 import LoginPage from "./LoginPage/LoginPage";
-
-const queryClient = new QueryClient();
 
 function Root() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="root">
-        <HashRouter>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
-        </HashRouter>
-      </div>
-    </QueryClientProvider>
+    <div className="root">
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </HashRouter>
+    </div>
   );
 }
 

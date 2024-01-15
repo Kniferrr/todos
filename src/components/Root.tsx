dcb5ecf3eaa75fb786de3da26575d546d8062849
Dmitry Pankov <pankov.dmitry.dev@gmail.com>
2024-01-15 16:24:48 +0300

    fix: change home page
    BREAKING CHANGE: Describe the breaking changes:

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,17 +1,17 @@
 import "./Root.scss";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./MainPage/MainPage";
 import LoginPage from "./LoginPage/LoginPage";
 
 function Root() {
   return (
     <div className="root">
-      <HashRouter>
+      <BrowserRouter basename="/app">
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
